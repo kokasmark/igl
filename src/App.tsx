6c865c7ac41042fac06078abc13f7621ebef6a4c
Kokas Márk <112559972+kokasmark@users.noreply.github.com>
2026-05-23 17:11:43 +0200

    Slicing of / on the pathname

--- src/App.tsx
+++ src/App.tsx
@@ -144,7 +144,7 @@ function App() {
       <div className='w-full'>
         <div style={{ display: "flex", alignItems: "center", gap: 12, marginBottom: 8 }}>
           {roomCode && <span className="badge badge-success cursor-pointer hover:scale-105 transition duration-300" 
-          onClick={()=> navigator.clipboard.writeText(`${window.location.host}${window.location.pathname}#${roomCode}`)}>
+          onClick={()=> navigator.clipboard.writeText(`${window.location.host}${window.location.pathname.slice(0, window.location.pathname.length - 1)}#${roomCode}`)}>
               {roomCode} <Icon icon='Copy' width={14} height={14} style={{marginLeft: 4}} fill='var(--color-text-success)'/>
             </span>}
           <span className="badge badge-neutral">Round {match.rounds.length}</span>
